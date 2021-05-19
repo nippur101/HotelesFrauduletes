c2232b19de5802d154073b8e6e09b7683330835a
Nippur <54045811+nippur101@users.noreply.github.com>
2021-05-19 17:11:06 -0300

    Panel Registro, recepcion y pago

--- Front/src/sample/Controller.java
+++ Front/src/sample/Controller.java
@@ -1,12 +1,22 @@
 package sample;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 
 public class Controller {
@@ -32,8 +42,66 @@ public class Controller {
     @FXML private AnchorPane paneModificarRecepcionist;
     @FXML private AnchorPane paneAgregarClient;
     @FXML private AnchorPane paneManejoClient;
-
+    @FXML private AnchorPane paneRegistro;
+    @FXML private AnchorPane paneReserva;
     @FXML private AnchorPane paneModificarClient;
+    @FXML private AnchorPane panePago;
+
+    @FXML private TableView<Detalle> tableViewDetalle;
+
+    @FXML private TableColumn<Detalle,String > columFecha;
+    @FXML private TableColumn<Detalle,Integer > columCuenta;
+    @FXML private TableColumn<Detalle,String > columNombreDeCuenta;
+    @FXML private TableColumn<Detalle,Integer > columReferencia;
+    @FXML private TableColumn<Detalle,String > columDescripcion;
+    @FXML private TableColumn<Detalle,Double > columCargos;
+    @FXML private TableColumn<Detalle,Double > columAbono;
+    @FXML private TableColumn<Detalle,Boolean > columEstado;
+
+    public void onPagoButtonClicked(MouseEvent event){
+
+        paneAdminPass.setVisible(false);
+        paneAgregarClient.setVisible(false);
+        paneModificarClient.setVisible(false);
+        paneManejoRecepcionist.setVisible(false);
+        paneAgregarRecepcionist.setVisible(false);
+        paneManejoClient.setVisible(false);
+        paneModificarRecepcionist.setVisible(false);
+        paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(true);
+
+        arrowAdmin.setVisible(false);
+        arrowClient.setVisible(false);
+        arrowHabitacion.setVisible(false);
+        arrowPago.setVisible(true);
+        arrowRecepcionista.setVisible(false);
+        arrowRegistro.setVisible(false);
+        arrowReserva.setVisible(false);
+
+        columFecha.setCellValueFactory(new PropertyValueFactory<Detalle,String>("Fecha"));
+        columCuenta.setCellValueFactory(new PropertyValueFactory<Detalle,Integer>("Cuenta"));
+        columNombreDeCuenta.setCellValueFactory(new PropertyValueFactory<Detalle,String>("Cuenta"));
+        columReferencia.setCellValueFactory(new PropertyValueFactory<Detalle,Integer>("Referencia"));
+        columDescripcion.setCellValueFactory(new PropertyValueFactory<Detalle,String>("Descripcion"));
+        columCargos.setCellValueFactory(new PropertyValueFactory<Detalle,Double>("Cargos"));
+        columAbono.setCellValueFactory(new PropertyValueFactory<Detalle,Double>("Abono"));
+        columEstado.setCellValueFactory(new PropertyValueFactory<Detalle,Boolean>("Estado"));
+
+        tableViewDetalle.setItems(getDetalle());
+
+
+    }
+
+
+    public ObservableList<Detalle>getDetalle(){
+       ObservableList<Detalle> detalles= FXCollections.observableArrayList();
+        detalles.add(new Detalle("02/04/2021",10,"hospedaje",104,"Renta Habitacion",5000.0,0,true));
+        detalles.add(new Detalle("03/04/2021",12,"telefono",104,"Servicio Telefono",400.0,0,true));
+        return detalles;
+    }
+
 
 
 
@@ -51,7 +119,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
-
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
         arrowAdmin.setVisible(true);
         arrowClient.setVisible(false);
         arrowHabitacion.setVisible(false);
@@ -71,6 +141,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(true);
@@ -90,6 +163,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(true);
@@ -108,6 +184,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(false);
@@ -127,6 +206,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(true);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(false);
@@ -147,6 +229,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(true);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(true);
         arrowClient.setVisible(false);
@@ -166,6 +251,9 @@ public class Controller {
         paneManejoClient.setVisible(false);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(false);
@@ -186,6 +274,9 @@ public class Controller {
         paneManejoClient.setVisible(true);
         paneModificarRecepcionist.setVisible(false);
         paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
         arrowAdmin.setVisible(false);
         arrowClient.setVisible(true);
@@ -198,16 +289,51 @@ public class Controller {
     }
 
     public void onReservaButtonCliked(MouseEvent event){
+        paneAdminPass.setVisible(false);
+        paneAgregarClient.setVisible(false);
+        paneModificarClient.setVisible(false);
+        paneManejoRecepcionist.setVisible(false);
+        paneAgregarRecepcionist.setVisible(false);
+        paneManejoClient.setVisible(true);
+        paneModificarRecepcionist.setVisible(false);
+        paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(true);
+        paneRegistro.setVisible(false);
+        panePago.setVisible(false);
 
+        arrowAdmin.setVisible(false);
+        arrowClient.setVisible(false);
+        arrowHabitacion.setVisible(false);
+        arrowPago.setVisible(false);
+        arrowRecepcionista.setVisible(false);
+        arrowRegistro.setVisible(false);
+        arrowReserva.setVisible(true);
     }
 
     public void onRegistroButtonClicked(MouseEvent event){
+        paneAdminPass.setVisible(false);
+        paneAgregarClient.setVisible(false);
+        paneModificarClient.setVisible(false);
+        paneManejoRecepcionist.setVisible(false);
+        paneAgregarRecepcionist.setVisible(false);
+        paneManejoClient.setVisible(true);
+        paneModificarRecepcionist.setVisible(false);
+        paneAccesoPermitido.setVisible(false);
+        paneReserva.setVisible(false);
+        paneRegistro.setVisible(true);
+        panePago.setVisible(false);
+
+        arrowAdmin.setVisible(false);
+        arrowClient.setVisible(false);
+        arrowHabitacion.setVisible(false);
+        arrowPago.setVisible(false);
+        arrowRecepcionista.setVisible(false);
+        arrowRegistro.setVisible(true);
+        arrowReserva.setVisible(false);
 
     }
 
-    public void onPagoButtonClicked(MouseEvent event){
 
-    }
 
     public void onHabitacionButtonClicked(MouseEvent event){
 
---
+++ Front/src/sample/Detalle.java
@@ -0,0 +1,92 @@
+package sample;
+
+import java.util.Date;
+
+public class Detalle {
+    private String fecha;
+    private int cuenta;
+    private String nombreCuenta;
+    private  int referencia;
+    private String descripcion;
+    private double cargos;
+    private double abono;
+    private boolean estado;
+
+    public Detalle() {
+    }
+
+    public Detalle(String fecha, int cuenta, String  nombreCuenta, int referencia, String descripcion, double cargos, double abono, boolean estado) {
+        this.fecha = fecha;
+        this.cuenta = cuenta;
+        this.nombreCuenta = nombreCuenta;
+        this.referencia = referencia;
+        this.descripcion = descripcion;
+        this.cargos = cargos;
+        this.abono = abono;
+        this.estado = estado;
+    }
+
+    public String  getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getNombreCuenta() {
+        return nombreCuenta;
+    }
+
+    public void setNombreCuenta(String nombreCuenta) {
+        this.nombreCuenta = nombreCuenta;
+    }
+
+    public int getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(int referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(double cargos) {
+        this.cargos = cargos;
+    }
+
+    public double getAbono() {
+        return abono;
+    }
+
+    public void setAbono(double abono) {
+        this.abono = abono;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
