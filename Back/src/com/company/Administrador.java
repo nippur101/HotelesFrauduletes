703b4d9a265fc0e6bfb3cc7a21dbc46fbd6dd55b
MFLOPEZ <marianofedericolopez@gmail.com>
2021-05-23 17:01:24 -0300

    Crea Recepcionista (Administrador)

--- Back/src/com/company/Administrador.java
+++ Back/src/com/company/Administrador.java
@@ -2,12 +2,26 @@ package com.company;
 
 public class Administrador extends UsuarioHotel{
 
-    ///Constructor
     public Administrador(String id, String nombreYapellido, String direccion, String correoElectronico, String telefono, String clave) {
         super(id, nombreYapellido, direccion, correoElectronico, telefono, clave);
     }
 
+
     ///Metodos
+
+    /// ---------------------- CREA USUARIO ----------------------
+    public Recepcionista creaRecepcionista (String id, String nombreYapellido, String direccion, String correoElectronico, String telefono, String clave){
+        Recepcionista a = new Recepcionista(id, nombreYapellido, direccion, correoElectronico,telefono,clave);
+        return a;
+    }
+
+    /// ---------------------- CREA HABITACION ----------------------
+    public Habitacion Habitacion (int numeroHabitacion, String tipoHabitacion, String caracteristicas, double precio){
+        Habitacion h = new Habitacion(numeroHabitacion, tipoHabitacion, caracteristicas, precio);
+        return h;
+    }
+
+
     @Override
     public String toString() {
         return "Administrador{" +
--- Back/src/com/company/Recepcionista.java
+++ Back/src/com/company/Recepcionista.java
@@ -2,7 +2,6 @@ package com.company;
 
 public class Recepcionista extends UsuarioHotel {
 
-    ///Constructor
     public Recepcionista(String id, String nombreYapellido, String direccion, String correoElectronico, String telefono, String clave) {
         super(id, nombreYapellido, direccion, correoElectronico, telefono, clave);
     }
